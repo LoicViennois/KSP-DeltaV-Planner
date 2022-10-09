61dd71800f1a2cd6d42dc5e89a201632aa8e0d0d
Loïc Viennois <contact@loicviennois.com>
2022-10-09 18:58:21 +0200

    feat: update PWA config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,8 +35,13 @@ import { AppRoutingModule } from './app-routing.module';
         NgbDropdownModule,
         NgbPopoverModule,
         NgbModalModule,
-        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-        AppRoutingModule
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
