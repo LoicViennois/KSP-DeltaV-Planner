3008fda586993c87745e892a9cf4ba5ead84ebdb
Gregor Altstädt <g.altstaedt@wirdesign.de>
2023-03-18 21:49:58 +0100

    Interactive SVG prototype Made the SVG itself interactive by adding click eventlisteners to AstroBody Elements which will set the flight path accordingly.

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -5,8 +5,10 @@ import { Subject } from 'rxjs';
 
 import { AstroPathService } from '../../services/astro-path.service';
 import { StepSelectionService } from '../../services/step-selection.service';
-import { AstroPath } from '../../models/planet.model';
+import { AstroPath, AstroBody } from '../../models/planet.model';
 import { Step, StepType } from '../../models/step.model';
+import { BodiesService } from '../../services/bodies.service';
+import { Kerbin } from 'src/app/models/data/kerbin';
 
 @Component({
   selector: 'ksp-map',
@@ -25,7 +27,8 @@ export class MapComponent implements OnInit, OnDestroy {
   private readonly suffixStepLow: string[];
 
   constructor(private readonly astroPathService: AstroPathService,
-              private readonly stepSelectionService: StepSelectionService) {
+              private readonly stepSelectionService: StepSelectionService,
+              private readonly bodiesService: BodiesService) {
     this.suffixHub = ['transit', 'hub', 'com'];
     this.suffixLow = this.suffixHub.concat(['transit-low', 'low']);
     this.suffixLanding = this.suffixLow.concat(['ground']);
@@ -35,8 +38,47 @@ export class MapComponent implements OnInit, OnDestroy {
     this.suffixStepLow = this.suffixStepSOI.concat(['transit-low', 'low']);
   }
 
+  get kerbin(): Kerbin {
+    return this.bodiesService.kerbin;
+  }
+  
+  private destinationSvgElementClicked(selection: AstroBody): void {
+    this.path.to = selection;  
+    if (this.path.to.name !== 'Kerbin') {
+      this.path.from = this.kerbin;
+    }
+    this.astroPathService.pathChanged(this.path);
+  }
+
+
+  /**
+   * Makes the SVG itself interactive.
+   * Click on a planet or moon will set the destination.
+   * Prototype status.
+   **/
+  addEventlistenersToAstroBodies() {
+    this.bodiesService.bodies.forEach(body => {
+      let bodyName = body.name
+      let lowerCaseBodyName = bodyName.toLowerCase()
+
+      let circleSelector = `#${lowerCaseBodyName}-ground use`
+      let bodyCircle = this.svg.select(circleSelector)
+      bodyCircle.on('click', () => {
+        this.destinationSvgElementClicked(body)
+      })
+
+      let labelSelector = `#${lowerCaseBodyName}`
+      let bodyLable = this.svg.select(labelSelector)
+      bodyLable.on('click', () => {
+        this.destinationSvgElementClicked(body)
+      })
+    })
+
+  }
+
   ngOnInit(): void {
     this.svg = d3.select('svg');
+    this.addEventlistenersToAstroBodies()
     this.astroPathService.getPath()
       .pipe(takeUntil(this.unsubscribe))
       .subscribe((p) => {
